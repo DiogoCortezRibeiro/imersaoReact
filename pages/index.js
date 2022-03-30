57c6dc2c6a54fcc85f122f8114aceee79c39c3dd
Diogo cortez <diogocortez@gmail.com>
2022-03-30 12:42:46 -0300

    Criando componentes do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,40 @@
-
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget/index';
+import GitHubCorner from '../src/components/GitHubCorner/index';
+import Footer from '../src/components/Footer/index';
+import QuizBackground from '../src/components/QuizBackground/index';
 
-const Tittle = styled.h1`
-  font-size: 50px;
-  color: black
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
   return (
-    <Tittle>My Page</Tittle>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+
+        <Widget>
+          <Widget.Header>teste titulo</Widget.Header>
+          <Widget.Content>testando</Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>teste titulo</Widget.Header>
+          <Widget.Content>testando</Widget.Content>
+        </Widget>
+
+        <Footer />
+
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://google.com.br" />
+    </QuizBackground>
   )
 }
